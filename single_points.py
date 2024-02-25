c258639ccb6bdf63a5d90ed8b6a5aaab449b3d22
Leif <leif.seute@gmx.de>
2024-02-25 10:08:31 +0100

    merging stuff

--- single_points.py
+++ single_points.py
@@ -15,8 +15,12 @@ import os
 
 ###################
 
+# define AlreadyBusy error type:
+class AlreadyBusy(Exception):
+    pass
 
-def calc_states(pdb_folder, n_states=None, memory=32, num_threads=4):
+
+def calc_states(pdb_folder, n_states=None, memory=32, num_threads=4, skip_if_busy=False):
     """
     Calculates the energies and forces for all states defined by positions.npy, atomic_numbers.npy and charge.npy in the given folder.
     """
@@ -32,8 +36,7 @@ def calc_states(pdb_folder, n_states=None, memory=32, num_threads=4):
     os.environ['PSI_SCRATCH'] = str(scratch)
 
 
-    ACCURACY = 0.1 #in kcal/mol
-
+    ACCURACY = 1e-2 # numerical threshold, in kcal/mol, we have 1kcal/mol ~50 meV
 
     if not memory is None and memory > 0:
         MEMORY = f'{int(memory)}GB'
@@ -50,6 +53,11 @@ def calc_states(pdb_folder, n_states=None, memory=32, num_threads=4):
 
     if not (pdb_folder/Path("positions.npy")).exists():
         return
+    
+    if skip_if_busy:
+        if (pdb_folder/Path("psi4_energies.npy")).exists() and (pdb_folder/Path("psi4_forces.npy")).exists():
+            log(f"skipping {pdb_folder.stem}, already (partially) calculated")
+            raise AlreadyBusy(f"skipping {pdb_folder.stem}, already (partially) calculated")
 
     positions = np.load(str(pdb_folder/Path("positions.npy")))
     atomic_numbers = np.load(str(pdb_folder/Path("atomic_numbers.npy")))
@@ -144,38 +152,56 @@ def calc_states(pdb_folder, n_states=None, memory=32, num_threads=4):
 
 
 
-def calc_all_states(folder, n_states=None, skip_errs=False, memory=32, num_threads=8):
+def calc_all_states(folder, n_states=None, skip_errs=False, memory=32, num_threads=8, permute_seed=None, is_cleanup_run:list=[False, True]):
     """
     For all folders in the given folder, call calc_states.
+    is_cleanup_run: list of bools. for every entry, iterates over all sub folders once. If True, the function will skip the folders where a calculation has already been started.
     """
     from pathlib import Path
+    import random
+
+    if permute_seed is not None:
+        random.seed(permute_seed)
 
     log = Logger(Path(folder), print_to_screen=True)
 
-    for i, pdb_folder in enumerate(Path(folder).iterdir()):
-        if pdb_folder.is_dir():
+
+
+    # iterate once with skip_if_busy=False and once with skip_if_busy=True (to finished the calculations that were potentially interrupted)
+    for skip_if_busy in is_cleanup_run:
+
+        skip_if_busy = not bool(skip_if_busy)
+
+        # also recalculate the folders in case something was added
+        pdb_folders = [f for f in Path(folder).iterdir() if f.is_dir()]
+        random.shuffle(pdb_folders)
+        log(f"calculating states for {len(pdb_folders)} folders in a first iteration.")
+
+        for i, pdb_folder in enumerate(pdb_folders):
             log("")
             log(f"calculating states for {i}")
             try:
-                calc_states(pdb_folder, n_states=n_states, memory=memory, num_threads=num_threads)
+                calc_states(pdb_folder, n_states=n_states, memory=memory, num_threads=num_threads, skip_if_busy=skip_if_busy)
             except KeyboardInterrupt:
                 raise
             except Exception as e:
+                if type(e) == AlreadyBusy:
+                    continue
                 if not skip_errs:
                     raise
-
                 log(f"failed to calculate states for {i} ({Path(folder).stem}): {type(e)}\n: {e}")
-                # raise
-                pass
-    log("finished!")
+
+        log(f"finished the iteration with skip_if_busy={skip_if_busy}.")
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Calculates states for a given folder.')
     parser.add_argument('folder', type=str, help='The folder containing the PDB files.')
     parser.add_argument('--n_states', '-n', type=int, help='The number of states to calculate.', default=None)
-    parser.add_argument('--skip_errs', '-s', action='store_true', help='Skip errors.', default=False)
+    parser.add_argument('--skip_errs', '-s', action='store_true', help='Skip errors.', default=True)
+    parser.add_argument('--permute_seed', '-p', type=int, help='The seed to use for shuffling the folders.', default=None)
     parser.add_argument('--memory', '-m', type=int, help='The amount of memory to use.', default=32)
     parser.add_argument('--num_threads', '-t', type=int, help='The number of threads to use.', default=4)
+    parser.add_argument('--cleanup_runs', '-c', type=bool, nargs='+', help='If True, the function will skip the folders where a calculation has already been started.', default=[False, True])
     args = parser.parse_args()
-    calc_all_states(folder=args.folder, n_states=args.n_states, skip_errs=args.skip_errs, memory=args.memory, num_threads=args.num_threads)
+    calc_all_states(folder=args.folder, n_states=args.n_states, skip_errs=args.skip_errs, memory=args.memory, num_threads=args.num_threads, permute_seed=args.permute_seed, is_cleanup_run=args.cleanup_runs)
